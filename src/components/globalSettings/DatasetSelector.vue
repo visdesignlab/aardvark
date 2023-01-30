62f0864c3c71c7f4c19d1cb94692eaf11c364bbc
Devin Lange <devinscottlange@gmail.com>
2023-01-30 15:54:27 -0700

    add basic ui for selecting imaging locations

--- src/components/globalSettings/DatasetSelector.vue
+++ src/components/globalSettings/DatasetSelector.vue
@@ -8,24 +8,63 @@
     /> -->
     <q-input
         ref="serverInputRef"
-        v-model="dataUrl"
+        v-model="datasetSelectionStore.serverUrl"
         filled
         type="url"
-        prefix="https://"
-        suffix="/aa_index.json"
-        hint="Dataset URL"
+        label="http://"
+        :suffix="datasetSelectionStore.entryPointFilename"
         debounce="1000"
-        lazy-rules="ondemand"
-        :rules="urlRules"
+        :loading="datasetSelectionStore.fetchingEntryFile"
+        :error="!datasetSelectionStore.serverUrlValid"
+        :error-message="datasetSelectionStore.errorMessage"
     />
+    <q-select
+        v-if="
+            datasetSelectionStore.serverUrlValid &&
+            datasetSelectionStore.serverUrl
+        "
+        label="Experiment"
+        v-model="datasetSelectionStore.currentExperimentFilename"
+        :options="datasetSelectionStore.experimentFilenameList"
+    />
+    <div
+        v-if="
+            datasetSelectionStore.currentExperimentMetadata &&
+            datasetSelectionStore.currentExperimentMetadata.locationMetadataList
+                .length > 0
+        "
+        class="mt-3"
+    >
+        <span>Imaging Locations</span>
+        <q-list bordered separator>
+            <q-item
+                v-for="location in datasetSelectionStore
+                    .currentExperimentMetadata?.locationMetadataList"
+                :key="location.id"
+                clickable
+                v-ripple
+                :active="location.show"
+                @click="location.show = !location.show"
+                ><q-item-section>{{ location.id }}</q-item-section></q-item
+            >
+        </q-list>
+    </div>
+    <!-- <hr />
+    <div>debug info</div>
+    <div>
+        {{ JSON.stringify(datasetSelectionStore.currentExperimentMetadata) }}
+    </div> -->
 </template>
 
 <script setup lang="ts">
 import { ref, watch, computed } from 'vue';
 import { useCellMetaData, type AnyAttributes } from '@/stores/cellMetaData';
+import { useDatasetSelectionStore } from '@/stores/datasetSelectionStore';
 import { parse, type ParseResult } from 'papaparse';
 
 const cellMetaData = useCellMetaData();
+const datasetSelectionStore = useDatasetSelectionStore();
+console.log(datasetSelectionStore.entryPointFilename);
 
 function onDataUpload(event: Event): void {
     const input = event.target as HTMLInputElement;
@@ -42,22 +81,22 @@ function onDataUpload(event: Event): void {
     });
 }
 const serverInputRef = ref<any>(null);
-const dataUrl = ref(null);
-const data = ref<{ experiments: string[] }>({ experiments: [] });
-watch(dataUrl, async () => {
-    console.log('data url change');
-    console.log({ url: dataUrl.value });
-    const response = await fetch('http://' + dataUrl.value + '/aa_index.json');
-    // response.status
-    data.value = await response.json();
-    serverInputRef?.value?.validate();
-    // console.log({ data.value });
-    // .then((response) => console.log({ blarg: response.json() }))
-    // .then((data) => console.log({ data }));
-});
+// const dataUrl = ref(null);
+// const data = ref<{ experiments: string[] }>({ experiments: [] });
+// watch(dataUrl, async () => {
+//     console.log('data url change');
+//     console.log({ url: dataUrl.value });
+//     const response = await fetch('http://' + dataUrl.value + '/aa_index.json');
+//     // response.status
+//     data.value = await response.json();
+//     serverInputRef?.value?.validate();
+//     // console.log({ data.value });
+//     // .then((response) => console.log({ blarg: response.json() }))
+//     // .then((data) => console.log({ data }));
+// });
 
-const urlValid = computed(() => data.value.experiments.length > 0);
-const urlRules = ref([() => urlValid.value || 'url is notr valuid']);
+// const urlValid = computed(() => data.value.experiments.length > 0);
+// const urlRules = ref([() => urlValid.value || 'url is notr valuid']);
 </script>
 
 <style scoped lange="scss"></style>
--- src/stores/datasetSelectionStore.ts
+++ src/stores/datasetSelectionStore.ts
@@ -1,6 +1,7 @@
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { defineStore } from 'pinia';
 import { v4 as uuidv4 } from 'uuid';
+import { useStorage } from '@vueuse/core';
 
 export interface ExperimentMetadata {
     name?: string; // user friendly name
@@ -23,28 +24,128 @@ export interface LocationMetadata {
     plate?: string;
     well?: string;
     location?: string;
+    show?: boolean; // if true, include data in interface
 }
 
-export const useDatasetSelectionStore = defineStore('globalSettings', () => {
-    const serverUrl = ref<string | null>(null);
-    const entryPointFilename = '/aa_index.json';
-    const experimentFilenameList = computed<string[]>(() => {
-        // todo
-        return [];
-    });
-    const currentExperimentFilename = ref<string | null>(null);
-    const currentExperimentMetadata = computed<ExperimentMetadata | null>(
-        () => {
-            // todo
-            return null;
-        }
-    );
+export const useDatasetSelectionStore = defineStore(
+    'datasetSelectionStore',
+    () => {
+        const entryPointFilename = '/aa_index.json';
 
-    return {
-        serverUrl,
-        entryPointFilename,
-        experimentFilenameList,
-        currentExperimentFilename,
-        currentExperimentMetadata,
-    };
-});
+        const serverUrlValid = ref(true);
+        const fetchingEntryFile = ref(false);
+        const errorMessage = ref('default error message');
+        let controller: AbortController;
+
+        const experimentFilenameList = ref<string[]>([]);
+        const currentExperimentFilename = ref<string | null>(null);
+        const currentExperimentMetadata = ref<ExperimentMetadata | null>(null);
+
+        const serverUrl = useStorage<string | null>('serverUrl', null);
+        if (serverUrl.value !== null && serverUrl.value !== '') {
+            fetchEntryFile();
+        }
+        watch(serverUrl, async () => {
+            fetchEntryFile();
+        });
+
+        async function fetchEntryFile(): Promise<void> {
+            console.log('data url change');
+            console.log({ url: serverUrl.value });
+            const fullURL = 'http://' + serverUrl.value + entryPointFilename;
+            if (controller) {
+                console.log('abort called');
+                controller.abort('stale request'); // cancel last fetch if it's still trying
+            }
+            controller = new AbortController();
+            fetchingEntryFile.value = true;
+            const response = await fetch(fullURL, {
+                signal: controller.signal, // link controller so can cancel if need to
+            }).catch((error: Error) => {
+                handleFetchEntryError(
+                    `Could not access ${fullURL}. "${error.message}"`
+                );
+            });
+            if (response == null) return;
+            if (!response.ok) {
+                handleFetchEntryError(
+                    `Server Error. "${response.status}: ${response.statusText}"`
+                );
+                return;
+            }
+            fetchingEntryFile.value = false;
+            serverUrlValid.value = true;
+            console.log({ response });
+            const data = await response.json();
+            console.log({ data });
+            experimentFilenameList.value = data.experiments;
+        }
+
+        function handleFetchEntryError(message: string): void {
+            // console.log('ERROR', errorMessage);
+            errorMessage.value = message;
+            serverUrlValid.value = false;
+            fetchingEntryFile.value = false;
+        }
+
+        watch(currentExperimentFilename, async () => {
+            fetchCurrentExperimentFile();
+        });
+
+        async function fetchCurrentExperimentFile(): Promise<void> {
+            console.log('experiment change');
+            console.log({ file: currentExperimentFilename.value });
+            const fullURL =
+                'http://' +
+                serverUrl.value +
+                '/' +
+                currentExperimentFilename.value;
+            // if (controller) {
+            //     console.log('abort called');
+            //     controller.abort('stale request'); // cancel last fetch if it's still trying
+            // }
+            // controller = new AbortController();
+            // fetchingEntryFile.value = true;
+            const response = await fetch(fullURL, {
+                // signal: controller.signal, // link controller so can cancel if need to
+            });
+            // .catch((error: Error) => {
+            //     handleFetchExperimentError(
+            //         `Could not access ${fullURL}. "${error.message}"`
+            //     );
+            // });
+            // if (response == null) return;
+            // if (!response.ok) {
+            //     handleFetchExperimentError(
+            //         `Server Error. "${response.status}: ${response.statusText}"`
+            //     );
+            //     return;
+            // }
+            // fetchingEntryFile.value = false;
+            // serverUrlValid.value = true;
+            console.log({ response });
+            const data = await response.json();
+            console.log({ data });
+            currentExperimentMetadata.value = data;
+        }
+
+        // function handleFetchExperimentError(message: string): void {
+        //     // console.log('ERROR', errorMessage);
+        //     errorMessage.value = message;
+        //     serverUrlValid.value = false;
+        //     fetchingEntryFile.value = false;
+        // }
+
+        return {
+            serverUrl,
+            serverUrlValid,
+            errorMessage,
+            fetchingEntryFile,
+            entryPointFilename,
+            experimentFilenameList,
+            currentExperimentFilename,
+            currentExperimentMetadata,
+            fetchEntryFile,
+        };
+    }
+);
