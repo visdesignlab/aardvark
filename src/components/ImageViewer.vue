38f0c1f558e4f4f6c292f6d4b4e8a5360fd6d2b5
Devin Lange <devinscottlange@gmail.com>
2023-02-09 16:34:56 -0700

    basic deck-gl working

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -1,6 +1,8 @@
 <template>
     <!-- <StubView></StubView> -->
-    <q-btn @click="loadStuff">load stuff</q-btn>
+    <canvas id="super-cool-unique-id" ref="deckGlContainer"></canvas>
+
+    <!-- <q-btn @click="loadStuff">load stuff</q-btn> -->
     <div
         v-if="dataLoaded"
         style="outline: solid 4px purple; position: relative; height: 50%"
@@ -10,11 +12,16 @@
             :views="vivViews"
             :viewStates="vivViewStates"
         ></VivViewerVue>
+
+        <!-- <DeckGLVue :ref="deckRef" :onViewStateChange="onViewStateChange"
+        :controller="true" :viewState="viewState" :views="[new
+        OrthographicView({ id: "ortho", controller: true })]} :layers="layers"
+        /> -->
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 // import { useCellMetaData, type Lineage } from '@/stores/cellMetaData';
 // import { useGlobalSettings } from '@/stores/globalSettings';
 import {
@@ -27,9 +34,48 @@ import {
 } from '@hms-dbmi/viv';
 
 import { applyPureReactInVue, applyReactInVue } from 'veaury';
+import { DeckGL } from 'deck.gl';
 import type { PixelData } from '@vivjs/types';
+import { Deck } from '@deck.gl/core';
+import { ScatterplotLayer } from '@deck.gl/layers';
+
+const INITIAL_VIEW_STATE = {
+    latitude: 37.8,
+    longitude: -122.45,
+    zoom: 15,
+};
+const deckGlContainer = ref(null);
+
+onMounted(() => {
+    console.log({ el: deckGlContainer.value });
+    const deckgl = new Deck({
+        initialViewState: INITIAL_VIEW_STATE,
+        // container: deckGlContainer.value.id,
+        canvas: deckGlContainer.value.id,
+        controller: true,
+        layers: [
+            new ScatterplotLayer({
+                data: [
+                    {
+                        position: [-122.451, 37.8],
+                        color: [255, 0, 0],
+                        radius: 100,
+                    },
+                    {
+                        position: [-122.45, 37.8],
+                        color: [0, 255, 0],
+                        radius: 100,
+                    },
+                ],
+                getColor: (d: any) => d.color,
+                getRadius: (d: any) => d.radius,
+            }),
+        ],
+    });
+});
 
 const VivViewerVue = applyReactInVue(VivViewer);
+const DeckGLVue = applyReactInVue(DeckGL);
 
 // const cellMetaData = useCellMetaData();
 // const globalSettings = useGlobalSettings();
