35f31b95fc58e9a4ff12e9c582a3bcabc06ef9c0
Devin Lange <devinscottlange@gmail.com>
2023-02-13 15:04:54 -0700

    first actual drawing of microscope image on screen!

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -31,48 +31,22 @@ import {
     VivViewer,
     getChannelStats,
     getDefaultInitialViewState,
+    ImageLayer,
 } from '@hms-dbmi/viv';
 
 import { applyPureReactInVue, applyReactInVue } from 'veaury';
 import { DeckGL } from 'deck.gl';
-import type { PixelData } from '@vivjs/types';
-import { Deck } from '@deck.gl/core';
+import type { PixelData, PixelSource, TiffPixelSource } from '@vivjs/types';
+import { Deck, OrthographicView } from '@deck.gl/core';
 import { ScatterplotLayer } from '@deck.gl/layers';
 
-const INITIAL_VIEW_STATE = {
-    latitude: 37.8,
-    longitude: -122.45,
-    zoom: 15,
-};
-const deckGlContainer = ref(null);
+import { Matrix4 } from '@math.gl/core';
 
-onMounted(() => {
-    console.log({ el: deckGlContainer.value });
-    const deckgl = new Deck({
-        initialViewState: INITIAL_VIEW_STATE,
-        // container: deckGlContainer.value.id,
-        canvas: deckGlContainer.value.id,
-        controller: true,
-        layers: [
-            new ScatterplotLayer({
-                data: [
-                    {
-                        position: [-122.451, 37.8],
-                        color: [255, 0, 0],
-                        radius: 100,
-                    },
-                    {
-                        position: [-122.45, 37.8],
-                        color: [0, 255, 0],
-                        radius: 100,
-                    },
-                ],
-                getColor: (d: any) => d.color,
-                getRadius: (d: any) => d.radius,
-            }),
-        ],
-    });
-});
+const INITIAL_VIEW_STATE = {
+    zoom: 0,
+    bearing: 0,
+    target: [767 / 2, 767 / 2, 0],
+};
 
 const VivViewerVue = applyReactInVue(VivViewer);
 const DeckGLVue = applyReactInVue(DeckGL);
@@ -137,6 +111,99 @@ async function loadStuff() {
     ];
     dataLoaded.value = true;
 }
+
+const deckGlContainer = ref(null);
+onMounted(async () => {
+    const loader = await loadMultiTiff(
+        // 'http://localhost:9001/michael-2/20221122_fs051_p9_mediaswitch_homebrew_A1_4_Phase.companion.ome',
+        [
+            [
+                { c: 0, t: 0, z: 0 },
+                'http://localhost:9001/michael-2/20221122_fs051_p9_mediaswitch_homebrew_A1_4_Phase1.tif',
+            ],
+        ]
+    );
+    const raster: PixelData = await loader.data[0].getRaster({
+        selection: { c: 0, t: 0, z: 0 },
+    });
+    const channelStats = getChannelStats(raster.data);
+
+    // const contrastLimits = [channelStats.contrastLimits];
+    const contrastLimits: [number, number][] = [[153, 539]];
+    // const contrastLimits = [{ begin: 153, end: 539 }];
+    console.log({ loader, channelStats, contrastLimits });
+    const selections = [{ c: 0, t: 0, z: 0 }];
+    const colors = [[255, 255, 255]];
+    const channelsVisible = [true];
+    // layoutConfig.value = [
+    //     {
+    //         loader: loader.data,
+    //         selections,
+    //         contrastLimits,
+    //         colors,
+    //         colormap: 'viridis',
+    //         channelsVisible,
+    //         width: 767,
+    //         height: 767,
+    //     },
+    // ];
+
+    vivViewStates.value = [
+        { target: [350, 350, 1], zoom: -0.1, id: 'test_viv_view' },
+        // getDefaultInitialViewState(loader.data, { width: 767, height: 767 }),
+    ];
+
+    const z = 1;
+    const overviewScale = 1;
+    const pixelSource = loader.data[0] as PixelSource<string>;
+    const imageLayer = new ImageLayer({
+        loader: pixelSource,
+        modelMatrix: new Matrix4().scale(2 ** z * overviewScale),
+        id: 'test-image-layer',
+        contrastLimits,
+        selections,
+        channelsVisible,
+    });
+    console.log({ el: deckGlContainer.value });
+    const deckgl = new Deck({
+        initialViewState: INITIAL_VIEW_STATE,
+        // container: deckGlContainer.value.id,
+        canvas: deckGlContainer.value.id,
+        controller: true,
+        layers: [
+            imageLayer,
+            new ScatterplotLayer({
+                id: 'test-scatterplot-layer',
+                data: [
+                    {
+                        position: [-122.451, 37.8],
+                        color: [255, 0, 0],
+                        radius: 10,
+                    },
+                    {
+                        position: [-122.45, 37.8],
+                        color: [0, 255, 0],
+                        radius: 10,
+                    },
+                    {
+                        position: [0, 0],
+                        color: [0, 0, 255],
+                        radius: 10,
+                    },
+                    {
+                        position: [767, 767],
+                        color: [255, 255, 0],
+                        radius: 10,
+                    },
+                ],
+                getColor: (d: any) => d.color,
+                getRadius: (d: any) => d.radius,
+            }),
+        ],
+
+        views: [new OrthographicView({ id: 'ortho', controller: true })],
+    });
+});
 </script>
 
 <style scoped lang="scss"></style>
