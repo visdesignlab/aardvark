62314a3c106a67e2fe5ef37a50d628024e32ec6a
Devin Lange <devinscottlange@gmail.com>
2023-02-22 14:08:24 -0700

    refactor provenance store

--- src/stores/provenanceStore.ts
+++ src/stores/provenanceStore.ts
@@ -6,96 +6,56 @@ import { initializeTrrack, Registry } from '@trrack/core';
 // import { toRaw } from 'vue';
 import { cloneDeep } from 'lodash-es';
 
+export interface SubStores {
+    [storeId: string]: Object;
+}
+
+export interface SkipStoreApplyTracker {
+    [storeId: string]: Boolean;
+}
+
+export interface RegisterActions {
+    [storeId: string]: any;
+    // [storeId: string]: ActionCreatorWithPayload<any, string>;
+}
+
 export const useProvenanceStore = defineStore('provenanceStore', () => {
-    const counterStore = useCounterStore();
-    const globalSettings = useGlobalSettings();
-    const gridstackLayoutStore = useGridstackLayoutStore();
-    // const store = useStore();
+    const storesToTrrack = [useGlobalSettings(), useGridstackLayoutStore()];
 
-    const initialState = {
-        counter: cloneDeep(counterStore.$state),
-        globalSettings: cloneDeep(globalSettings.$state),
-        layoutConfig: cloneDeep(gridstackLayoutStore.$state),
-        // store: cloneDeep(store.$state),
-    };
-
-    const skipApply = {
-        counter: false,
-        globalSettings: false,
-        layoutConfig: false,
-        // store: false,
-    };
-
+    const initialState: SubStores = {};
+    const skipApply: SkipStoreApplyTracker = {};
+    const registerActions: RegisterActions = {};
     const registry = Registry.create();
-    const updateCounter = registry.register(
-        'update counter',
-        (trrackState, newCounterState) => {
-            trrackState.counter = cloneDeep(newCounterState);
-        }
-    );
-    const updateGlobalSettings = registry.register(
-        'update global settings',
-        (trrackState, newGlobalSettings) => {
-            // console.log(cloneDeep(trrackState));
-            trrackState.globalSettings = newGlobalSettings;
-        }
-    );
+    for (const store of storesToTrrack) {
+        const storeId = store.$id;
+        initialState[storeId] = cloneDeep(store.$state);
+        skipApply[storeId] = false;
+        registerActions[storeId] = registry.register(
+            `update ${storeId}`,
+            (trrackState, newPiniaState) => {
+                trrackState[storeId] = cloneDeep(newPiniaState);
+            }
+        );
+    }
 
-    const updateLayoutConfig = registry.register(
-        'update layoutConfig',
-        (trrackState, newStore) => {
-            // console.log('in action');
-            // console.log({ trrackState });
-            trrackState.layoutConfig = cloneDeep(newStore);
-        }
-    );
-    // const updateStore = registry.register(
-    //     'update store',
-    //     (trrackState, newStore) => {
-    //         trrackState.store = cloneDeep(newStore);
-    //     }
-    // );
     const provenance = initializeTrrack({
         initialState,
         registry,
     });
 
-    counterStore.$subscribe((mutation, state) => {
-        if (skipApply.counter) {
-            skipApply.counter = false;
-            return;
-        }
-        provenance.apply('counter changed', updateCounter(state));
-    });
-    globalSettings.$subscribe((_mutation, state) => {
-        if (skipApply.globalSettings) {
-            skipApply.globalSettings = false;
-            return;
-        }
-        provenance.apply(
-            'global settings change',
-            updateGlobalSettings(cloneDeep(state))
-        );
-    });
-    gridstackLayoutStore.$subscribe((_mutation, state) => {
-        // console.log('layout sub');
-        if (skipApply.layoutConfig) {
-            skipApply.layoutConfig = false;
-            return;
-        }
-        // console.log({ layout: cloneDeep(state) });
-        provenance.apply(
-            'layout store settings change',
-            updateLayoutConfig(cloneDeep(state))
-        );
-    });
-    // store.$subscribe((_mutation, state) => {
-    //     if (skipApply.store) {
-    //         skipApply.store = false;
-    //         return;
-    //     }
-    //     provenance.apply('store settings change', updateStore(state));
-    // });
+    for (const store of storesToTrrack) {
+        store.$subscribe((mutation, state) => {
+            const storeId = mutation.storeId;
+            if (skipApply[storeId]) {
+                skipApply[storeId] = false;
+                return;
+            }
+            provenance.apply(
+                `${storeId} changed`,
+                registerActions[storeId](state)
+            );
+        });
+    }
 
     const nodeIds = new Set<string>([provenance.root.id]);
     provenance.currentChange(() => {
@@ -103,16 +63,13 @@ export const useProvenanceStore = defineStore('provenanceStore', () => {
         const jumpedToNode: boolean = nodeIds.has(provNodeId);
         if (jumpedToNode) {
             for (const key in skipApply) {
-                (skipApply as any)[key] = true;
+                skipApply[key] = true;
             }
-            counterStore.$state = cloneDeep(provenance.getState().counter);
-            globalSettings.$state = cloneDeep(
-                provenance.getState().globalSettings
-            );
-            gridstackLayoutStore.$state = cloneDeep(
-                provenance.getState().layoutConfig
-            );
-            // store.$state = cloneDeep(provenance.getState().store);
+            for (const store of storesToTrrack) {
+                store.$state = cloneDeep(
+                    provenance.getState()[store.$id]
+                ) as any;
+            }
         }
         nodeIds.add(provNodeId);
     });
