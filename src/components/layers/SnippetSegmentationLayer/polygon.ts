478bee635304ed7f2588fc010723f7338d0931fc
Devin Lange <devinscottlange@gmail.com>
2024-03-05 13:35:56 -0700

    update license comments

--- src/components/layers/SnippetSegmentationLayer/polygon.ts
+++ src/components/layers/SnippetSegmentationLayer/polygon.ts
@@ -1,3 +1,6 @@
+// Modified from the SolidPolygonLayer included in deck.gl.
+// the following is the orginal license for the SolidPolygonLayer:
+//
 // Copyright (c) 2015 - 2017 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
