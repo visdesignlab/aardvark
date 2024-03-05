a10d83d8963960bb3c558f7f04f8509d9c77ae3f
Devin Lange <devinscottlange@gmail.com>
2024-03-05 13:25:48 -0700

    prettier format

--- src/components/layers/path-layer/path.ts
+++ src/components/layers/path-layer/path.ts
@@ -1,7 +1,10 @@
-import {cutPolylineByGrid, cutPolylineByMercatorBounds} from '@math.gl/polygon';
+import {
+    cutPolylineByGrid,
+    cutPolylineByMercatorBounds,
+} from '@math.gl/polygon';
 
-import type {NumericArray} from '@math.gl/core';
-import type {Position} from '@deck.gl/core';
+import type { NumericArray } from '@math.gl/core';
+import type { Position } from '@deck.gl/core';
 
 export type NestedPathGeometry = Position[];
 export type FlatPathGeometry = NumericArray;
@@ -14,28 +17,28 @@ export type NormalizedPathGeometry = FlatPathGeometry[] | FlatPathGeometry;
  * Returns a flat array of path positions, or a list of flat arrays representing multiple paths
  */
 export function normalizePath(
-  path: PathGeometry,
-  size: number,
-  gridResolution?: number,
-  wrapLongitude?: boolean
+    path: PathGeometry,
+    size: number,
+    gridResolution?: number,
+    wrapLongitude?: boolean
 ): number[][] | NumericArray {
-  let flatPath: NumericArray;
-  if (Array.isArray(path[0])) {
-    const length = path.length * size;
-    flatPath = new Array(length);
-    for (let i = 0; i < path.length; i++) {
-      for (let j = 0; j < size; j++) {
-        flatPath[i * size + j] = path[i][j] || 0;
-      }
+    let flatPath: NumericArray;
+    if (Array.isArray(path[0])) {
+        const length = path.length * size;
+        flatPath = new Array(length);
+        for (let i = 0; i < path.length; i++) {
+            for (let j = 0; j < size; j++) {
+                flatPath[i * size + j] = path[i][j] || 0;
+            }
+        }
+    } else {
+        flatPath = path as NumericArray;
     }
-  } else {
-    flatPath = path as NumericArray;
-  }
-  if (gridResolution) {
-    return cutPolylineByGrid(flatPath, {size, gridResolution});
-  }
-  if (wrapLongitude) {
-    return cutPolylineByMercatorBounds(flatPath, {size});
-  }
-  return flatPath;
+    if (gridResolution) {
+        return cutPolylineByGrid(flatPath, { size, gridResolution });
+    }
+    if (wrapLongitude) {
+        return cutPolylineByMercatorBounds(flatPath, { size });
+    }
+    return flatPath;
 }
