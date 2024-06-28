d18dee5a1c345f388f20b74660bad22622f485a7
Brian Bollen <briancbollen@gmail.com>
2024-06-28 16:19:34 -0600

    Added collapse button to sidebar

--- apps/client/src/components/globalSettings/GlobalSettingsView.vue
+++ apps/client/src/components/globalSettings/GlobalSettingsView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { router } from '@/router';
-import { useGlobalSettings } from '@/stores/globalSettings';
+import { type SettingsPage, useGlobalSettings } from '@/stores/globalSettings';
 import { onKeyStroke } from '@vueuse/core';
 import { ref } from 'vue';
 const globalSettings = useGlobalSettings();
@@ -12,22 +12,28 @@ onKeyStroke(['b', 'B'], (e: KeyboardEvent) => {
 
 const tab = ref('Select Dataset Location');
 
-const navigateToPage = (url:string|undefined) => {
-    if(url){
-        router.push(url);
+const handleIconClick = (setting:SettingsPage) => {
+    if(setting.url){
+        router.push(setting.url);
     }
+    isPanelVisible.value = true;
+}
+
+const isPanelVisible = ref(true);
+const collapse = () => {
+    isPanelVisible.value = false;
 }
 </script>
 
 <template>
-    <div class="d-flex h-100 ps-1 pt-1 pb-1">
+    <div class="d-flex h-100 ps-1 pt-1 pb-1  flex-grow-1">
       <q-tabs
         v-model="tab"
         vertical
         class="fixed-width-tabs justify-content-start"
       >
         <template v-for="setting in globalSettings.settingsPages" :key="setting.name">
-            <q-tab :name="setting.name" @click="navigateToPage(setting.url)">
+            <q-tab :name="setting.name" @click="handleIconClick(setting)">
                 <template v-slot:default>
                     <div class="d-flex align-items-center">
                         <font-awesome-icon :icon="`fa-solid ${setting.faKey}`" style="height:1.3em;" />
@@ -37,24 +43,37 @@ const navigateToPage = (url:string|undefined) => {
         </template>
       </q-tabs>
   
-      <q-tab-panels
-        v-model="tab"
-        class="fixed-width-panels"
-        :dark="globalSettings.darkMode"
-        swipeable
-        vertical
-        style="margin-left:5px;"
-      >
-        <template v-for="setting in globalSettings.settingsPages" :key="setting.name">
-          <q-tab-panel :name="setting.name" style="border: 1px solid rgba(0,0,0,0.12);border-radius:4px;">
-            <div v-if="globalSettings.activePage != null && !globalSettings.activePage.disableSidebar">
-              <h5>{{ setting.name }}</h5>
-              <hr />
-              <component :is="setting.component"></component>
-            </div>
-          </q-tab-panel>
-        </template>
-      </q-tab-panels>
+    <div class="d-flex flex-column">
+        <transition name="collapse">
+            <q-tab-panels
+                v-model="tab"
+                v-if="isPanelVisible"
+                class="fixed-width-panels flex-grow-1"
+                :dark="globalSettings.darkMode"
+                swipeable
+                vertical
+                style="margin-left:5px;"
+            >
+                <template v-for="setting in globalSettings.settingsPages" :key="setting.name">
+                <q-tab-panel :name="setting.name" style="border: 1px solid rgba(0,0,0,0.12);border-radius:4px;">
+                    <div v-if="globalSettings.activePage != null && !globalSettings.activePage.disableSidebar">
+                        <div class="d-flex row justify-space-between align-center" style="margin-bottom:5px;">
+                            <h5 style="margin-bottom:0px;">{{ setting.name }}</h5>
+                            <q-icon
+                                name="mdi-arrow-collapse-left"
+                                size="15px"
+                                @click="collapse()"
+                            />
+                        </div>
+                        <hr />
+                        <component :is="setting.component"></component>
+                    </div>
+                </q-tab-panel>
+                </template>
+            </q-tab-panels>
+        </transition>
+    </div>
+
     </div>
   </template>
   
@@ -64,6 +83,9 @@ $panel-width: 300px;
 
 .flex-grow-0 {
     flex-grow: 0;
+}
+.flex-grow-1{
+    flex-grow:1;
 }
 
 .fixed-width-panels{
