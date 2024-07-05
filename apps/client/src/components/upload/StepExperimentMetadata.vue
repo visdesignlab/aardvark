0daab21a30a4c58fbcc7ef3d3f5428e616af3c19
Devin Lange <devinscottlange@gmail.com>
2024-07-05 17:22:55 -0600

    cleanup metadata step, add warning about filename uniqueness.

--- apps/client/src/components/upload/StepExperimentMetadata.vue
+++ apps/client/src/components/upload/StepExperimentMetadata.vue
@@ -1,59 +1,24 @@
 <script setup lang="ts">
-import { ref, computed, watch, onMounted } from 'vue';
 import { useUploadStore } from '@/stores/uploadStore';
 const uploadStore = useUploadStore();
-
-// import { defineProps, defineEmits } from 'vue';
-
-// const props = defineProps<{
-//     experimentName: string;
-//     numberOfLocations: number;
-// }>();
-
-// const emits = defineEmits<{
-//     (event: 'update:experimentName', value: string): void;
-//     (event: 'update:numberOfLocations', value: string): void;
-// }>();
-
-// const updateExperimentNameValue = (value: string | number | null) => {
-//     console.log('update Exp value is called');
-//     emits('update:experimentName', value?.toString() ?? '');
-// };
-
-// const updateNumberOfLocationsValue = (value: string | number | null) => {
-//     console.log('update location value is called');
-//     emits('update:numberOfLocations', value?.toString() ?? '');
-// };
 </script>
 
 <template>
-    <!-- <span class="step-title"> Create Your Experiment </span> -->
-    <div class="column q-mt-sm q-gutter-lg">
-        <q-input
-            v-model="uploadStore.experimentName"
-            outlined
-            style="width: 500px"
-            label-slot
-            :rules="[(val) => !!val || 'Field is required']"
-            label="Experiment Name"
-        >
-        </q-input>
-        <!-- <q-input
-            :model-value="numberOfLocations"
-            @update:model-value="updateNumberOfLocationsValue"
-            outlined
-            style="width: 500px"
-            label-slot
-            :rules="[
-                (val) => !!val || 'Field is required',
-                (val) =>
-                    !isNaN(parseInt(val)) || 'Please enter in a valid number.',
-            ]"
-            hint="Number of imaging locations that will be added."
-            label="Number of Locations"
-        > -->
-        <!-- </q-input> -->
+    <div>
+        Select experiment metadata. The experiment name must be unique. We don't
+        currently check this for you. Including today's data in the name is one
+        good way to help ensure uniqueness.
     </div>
+    <q-input
+        class="q-mt-md"
+        v-model="uploadStore.experimentName"
+        outlined
+        style="width: 500px"
+        label-slot
+        :rules="[(val) => !!val || 'Field is required']"
+        label="Experiment Name"
+    >
+    </q-input>
 </template>
 
 <style scoped lang="scss"></style>
